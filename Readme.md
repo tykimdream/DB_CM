e95beef7ecf585838f2fbf932ec16d0dd9351626
tykimdream@korea.ac.kr <tykimdream@korea.ac.kr>
2021-11-23 01:38:59 +0900

    css

--- Readme.md
+++ Readme.md
@@ -18,3 +18,4 @@ DB 작성 완료, import 완료
 11.25
 DB Data 추가
 sql문 작성
+StudentInfo.php 페이지 구현
--- studentInfo.php
+++ studentInfo.php
@@ -12,33 +12,48 @@
 </head>
 
 <body onload="startTime()">
-    <h1>학생 정보 조회</h1>
+    <span class="subTitle">학생 정보 조회</span>
+    <span class="returnButton" type="button" onclick="location.href='Landing.php'">돌아가기</span>
     <div id="clock"></div>
-
     <!-- 학번   이름    학과    주소    전화번호    백신여부(미접, 1차, 2차)    발열여부(o,x) -->
     <!-- 발열여부, 백신 상태 업데이트 가능하게 만들기 -->
     <!-- 검색 기능: 백신 비접종, 1차, 2차에 해당하는 학생 보여주기 -->
+    <div class="tableDiv">
+        <table>
+            <tr class="tableTitle">
+                <td>학번</td>
+                <td>이름</td>
+                <td>학과</td>
+                <td>학년</td>
+                <td>거주 지역</td>
+                <td>전화번호</td>
+                <td>접종 여부</td>
+                <td>발열 여부</td>
+            </tr>
+            <?
+            $connect = mysqli_connect("localhost", "root", "1234");
+            mysqli_select_db($connect, "cm");
 
-    <?
-    $connect = mysqli_connect("localhost", "root", "1234");
-    mysqli_select_db($connect, "cm");
+            mysqli_query($connect, "set session character_set_connection=utf8;");
+            mysqli_query($connect, "set session character_set_results=utf8;");
+            mysqli_query($connect, "set session character_set_client=utf8;");
 
-    $sql = "select * from card1;";
-    $result = mysqli_query($connect, $sql);
-    $fields = mysqli_num_fields($result);
+            $sql = "select * from student";
+            $result = mysqli_query($connect, $sql);
+            $fields = mysqli_num_fields($result);
 
-    while ($row = mysqli_fetch_row($result)) {
-        echo "<tr>";
-        for ($i = 0; $i < $fields; $i = $i + 1) {
-            echo "<td>$row[$i]</td>";
-        }
-        echo "</tr>";
-    }
-    mysqli_close($connect);
-    ?>
+            while ($row = mysqli_fetch_row($result)) {
+                echo "<tr>";
+                for ($i = 0; $i < $fields; $i = $i + 1) {
+                    echo "<td>$row[$i]</td>";
+                }
+                echo "</tr>";
+            }
+            mysqli_close($connect);
+            ?>
+        </table>
+    </div>
 
-
-    <button class="button" type="button" onclick="location.href='Landing.php'">학과별</button>
 
 </body>
 
